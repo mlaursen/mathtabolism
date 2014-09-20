337bf6ffcf148cebaf9b9e58f934152a4fa71186
mlaursen <mlaursen03@gmail.com>
2014-09-20 13:10:52 -0400

    Starting TDEE

--- Source/java/com/github/mlaursen/mathtabolism/nutrition/formula/TotalDailyEnergyExpenditure.java
+++ Source/java/com/github/mlaursen/mathtabolism/nutrition/formula/TotalDailyEnergyExpenditure.java
@@ -6,6 +6,9 @@ package com.github.mlaursen.mathtabolism.nutrition.formula;
 import com.github.mlaursen.mathtabolism.constants.Gender;
 import com.github.mlaursen.mathtabolism.constants.TDEEFormula;
 import com.github.mlaursen.mathtabolism.nutrition.Calorie;
+import com.github.mlaursen.mathtabolism.unit.Measurement;
+import com.github.mlaursen.mathtabolism.unit.UnitConverter;
+import com.github.mlaursen.mathtabolism.unit.UnitMeasurement;
 import com.github.mlaursen.mathtabolism.unit.UnitSystem;
 
 /**
@@ -18,6 +21,11 @@ public class TotalDailyEnergyExpenditure {
 	
 	public TotalDailyEnergyExpenditure(double weight, double height, int age, Gender gender, UnitSystem unitSystem,
 			TDEEFormula formula) {
+		Measurement weightM = new Measurement(unitSystem.isMetric() ? UnitMeasurement.KILOGRAM : UnitMeasurement.POUND, weight);
+		Measurement heightM = new Measurement(unitSystem.isMetric() ? UnitMeasurement.CENTIMETER : UnitMeasurement.INCH, height);
+		
+		UnitConverter.convert(weightM, UnitMeasurement.KILOGRAM);
+		UnitConverter.convert(heightM, UnitMeasurement.CENTIMETER);
 		double weightInKG = 0; // UnitConverter.convert(weight, MeasuringUnit.METRIC)
 		double heightInCM = 0; // UnitConverter.convert(height, MeasuringUnit.METRIC)
 		this.tdee = formula.calculate(weightInKG, heightInCM, age, gender);
