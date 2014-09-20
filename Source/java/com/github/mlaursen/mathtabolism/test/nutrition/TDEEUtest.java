5f55ca3df522b5d858e0afa92926b85b39d64462
mlaursen <mlaursen03@gmail.com>
2014-09-20 14:02:44 -0400

    TDEE and BMR Tests

--- Source/java/com/github/mlaursen/mathtabolism/test/nutrition/TDEEUtest.java
+++ Source/java/com/github/mlaursen/mathtabolism/test/nutrition/TDEEUtest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import com.github.mlaursen.mathtabolism.constants.Gender;
 import com.github.mlaursen.mathtabolism.constants.TDEEFormula;
 import com.github.mlaursen.mathtabolism.nutrition.Calorie;
-import com.github.mlaursen.mathtabolism.nutrition.formula.TotalDailyEnergyExpenditure;
+import com.github.mlaursen.mathtabolism.nutrition.formula.FormulaCalculation;
 import com.github.mlaursen.mathtabolism.unit.UnitSystem;
 
 /**
@@ -21,16 +21,28 @@ import com.github.mlaursen.mathtabolism.unit.UnitSystem;
 public class TDEEUtest {
 	
 	@Test
-	public void testNewTDEE() {
-		TotalDailyEnergyExpenditure tdee = new TotalDailyEnergyExpenditure(183, 72, 23, Gender.MALE, UnitSystem.IMPERIAL, TDEEFormula.HARRIS_BENEDICT);
-		assertThat(tdee.getTdee(), is(new Calorie(1966.97)));
-		tdee = new TotalDailyEnergyExpenditure(83.0074, 182.88, 23, Gender.MALE, UnitSystem.METRIC, TDEEFormula.HARRIS_BENEDICT);
-		assertThat(tdee.getTdee(), is(new Calorie(1966.97)));
+	public void testCalculateTDEE() {
+		Calorie tdee = FormulaCalculation.calculateTDEE(183, 72, 23, Gender.MALE, UnitSystem.IMPERIAL, TDEEFormula.HARRIS_BENEDICT);
+		assertThat(tdee, is(new Calorie(1966.97)));
+		tdee = FormulaCalculation.calculateTDEE(83.0074, 182.88, 23, Gender.MALE, UnitSystem.METRIC, TDEEFormula.HARRIS_BENEDICT);
+		assertThat(tdee, is(new Calorie(1966.97)));
 		
-		tdee = new TotalDailyEnergyExpenditure(183, 72, 23, Gender.MALE, UnitSystem.IMPERIAL, TDEEFormula.MIFFLIN_ST_JOER);
-		assertThat(tdee.getTdee(), is(new Calorie(1863.07)));
-		tdee = new TotalDailyEnergyExpenditure(83.0074, 182.88, 23, Gender.MALE, UnitSystem.METRIC, TDEEFormula.MIFFLIN_ST_JOER);
-		assertThat(tdee.getTdee(), is(new Calorie(1863.07)));
+		tdee = FormulaCalculation.calculateTDEE(183, 72, 23, Gender.MALE, UnitSystem.IMPERIAL, TDEEFormula.MIFFLIN_ST_JOER);
+		assertThat(tdee, is(new Calorie(1863.07)));
+		tdee = FormulaCalculation.calculateTDEE(83.0074, 182.88, 23, Gender.MALE, UnitSystem.METRIC, TDEEFormula.MIFFLIN_ST_JOER);
+		assertThat(tdee, is(new Calorie(1863.07)));
 	}
 	
+	@Test
+	public void testCalculateBMR() {
+		Calorie bmr = FormulaCalculation.calculateBMR(183, 72, 23, Gender.MALE, UnitSystem.IMPERIAL);
+		assertThat(bmr, is(new Calorie(1964.09)));
+		bmr = FormulaCalculation.calculateBMR(83, 182, 23, Gender.MALE, UnitSystem.METRIC);
+		assertThat(bmr, is(new Calorie(1956.69)));
+		
+		bmr = FormulaCalculation.calculateBMR(120, 62, 23, Gender.FEMALE, UnitSystem.IMPERIAL);
+		assertThat(bmr, is(new Calorie(1360.30)));
+		bmr = FormulaCalculation.calculateBMR(66.5, 157, 23, Gender.FEMALE, UnitSystem.METRIC);
+		assertThat(bmr, is(new Calorie(1467.9)));
+	}
 }
