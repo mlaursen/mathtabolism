5d9fd3de78cb7c69d7ac1f6f4acc6ecf48e6777d
mlaursen <mlaursen03@gmail.com>
2014-12-20 12:15:26 -0500

    Finsihed Username/Password Validation - Hopefully
    Added some mroe front end validation for username and password.
    Displaying mroe errors now..

--- Source/webapp/resources/js/accountValidation.js
+++ Source/webapp/resources/js/accountValidation.js
@@ -3,6 +3,8 @@
  */
 var form = $("#signup-form");
 var user = $("input[id$=':username']");
+var userErr = $("span[id$=':username-exists']");
+var userImg = $("img[id$=':checklist-username']")
 var pswd = $("input[id$=':password']");
 var pswdErr = $("#password-error");
 var pswdImg = $("img[id$=':checklist-password']");
@@ -10,43 +12,77 @@ var conf = $("input[id$=':confirm-password']");
 var confErr = $("#confirm-password-error");
 var confImg = $("img[id$=':checklist-confirm-password']");
 
-
-form.submit(function() {
-  var isUsernameSet = user.val() != '';
-  return isUsernameSet && isMatchingPasswords() && pswd.val().length > 1;
-});
-
-function isMatchingPasswords() {
-  if(pswd.val() !== conf.val()) {
-    if(!pswdErr.hasClass("visible")) {
-      pswdErr.addClass("visible");
-    }
-    
-    if(!confErr.hasClass("visible")) {
-      confErr.addClass("visible");
-    }
-    pswdErr.html(passwordMatchError);
-    confErr.html(passwordMatchError);
-    
-    pswdImg.attr("src", cross);
-    confImg.attr("src", cross);
-    
-    if(pswdImg.hasClass("hidden")) {
-      pswdImg.removeClass("hidden");
-      confImg.removeClass("hidden");
-    }
+function isValidUsername() {
+  if(user.val() == '') {
+    addUsernameError(usernameError);
     return false;
   } else {
-    pswdErr.removeClass("visible");
-    pswdErr.html("");
-    confErr.removeClass("visible");
-    confErr.html("");
-    pswdImg.attr("src", checkmark);
-    confImg.attr("src", checkmark);
+    return !$("span[id$=':username-exists']").hasClass("visible");
+  }
+}
+
+function addUsernameError(msg) {
+  if(!userErr.hasClass("visible")) {
+    userErr.addClass("visible");
+  }
+  
+  if(userImg.hasClass("hidden")) {
+    userImg.removeClass("hidden");
+  }
+  
+  userImg.attr("src", cross);
+  
+  userErr.html(msg);
+}
+
+function addPasswordError(msg) {
+  if(!pswdErr.hasClass("visible")) {
+    pswdErr.addClass("visible");
+  }
+  
+  if(!confErr.hasClass("visible")) {
+    confErr.addClass("visible");
+  }
+  
+  if(pswdImg.hasClass("hidden") || confImg.hasClass("hidden")) {
+    pswdImg.removeClass("hidden");
+    confImg.removeClass("hidden");
+  }
+
+  pswdImg.attr("src", cross);
+  confImg.attr("src", cross);
+  pswdErr.html(msg);
+  confErr.html(msg);
+}
+
+function removePasswordErrors() {
+  pswdErr.removeClass("visible");
+  pswdErr.html("");
+  confErr.removeClass("visible");
+  confErr.html("");
+  pswdImg.attr("src", checkmark);
+  confImg.attr("src", checkmark);
+}
+
+function isValidPassword() {
+  if(pswd.val() == '') {
+    addPasswordError(passwordError);
+    return false;
+  } else if(pswd.val() !== conf.val()) {
+    addPasswordError(passwordMatchError);
+    return false;
+  } else {
+    removePasswordErrors();
     return true;
   }
 }
 
+form.submit(function() {
+  var isValid = isValidUsername();
+  isValid = isValidPassword() && isValid;
+  return isValid;
+});
+
 conf.keyup(function() {
-  isMatchingPasswords();
+  isValidPassword();
 });
